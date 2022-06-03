f26b4b817cbc627dc2aec5216093222f70aa03b8
Maycon Mendes <mayconsouzamendes@gmail.com>
2022-06-03 14:36:41 -0300

    Scroll animation Overload
    Adding scroll animation overload to our project.
    Create new scene;
    Create new animation;
    * javascript
    * css

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let controller;
 let slideScene;
+let pageScene;
 
 function animateSlides() {
     // Init Controller
@@ -10,7 +11,7 @@ function animateSlides() {
     const nav = document.querySelector('.nav-header');
 
     // Loop over each slide
-    sliders.forEach((slide) => {
+    sliders.forEach((slide, index, slides) => {
         const revealImg = slide.querySelector('.reveal-img');
         const img = slide.querySelector('img');
         const revealText = slide.querySelector('.reveal-text');
@@ -23,6 +24,44 @@ function animateSlides() {
         slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, '-=1');
         slideTl.fromTo(revealText, { x: '0%'}, { x: '100%'}, '-=0.75');
         slideTl.fromTo(nav, { y: '-100%' }, { y: '0%' }, '-=0.9');
+
+        // Create Scene
+        slideScene = new ScrollMagic.Scene({
+            triggerElement: slide,
+            triggerHook: 0.25,
+            reverse: false
+        })
+        .setTween(slideTl)
+        .addIndicators({
+            colorStart: 'white',
+            colorTrigger: 'white',
+            name: 'slide'
+        })
+        .addTo(controller);
+
+        // New animation
+        const pageTl = gsap.timeline();
+        let nextSlide = slides.length - 1 === index ? 'end' : slides[index + 1];
+        pageTl.fromTo(nextSlide, { y: '0%'}, { y: '50%' });
+        pageTl.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5 });
+        pageTl.fromTo(nextSlide, { y: '50%'}, { y: '0%' });
+
+        // Create new scene
+        pageScene = new ScrollMagic.Scene({
+            triggerElement: slide,
+            duration: '100%',
+            triggerHook: 0
+        })
+        .addIndicators({
+            colorStart: 'white',
+            colorTrigger: 'white',
+            name: 'page',
+            indent: 200
+        })
+        .setPin(slide, { pushFollowers: false })
+        .setTween(pageTl)
+        .addTo(controller);
+
     })
     
 }
