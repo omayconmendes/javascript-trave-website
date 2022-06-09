f0043aadf0cc415de89e371d753c03dbace47e81
Maycon Mendes <mayconsouzamendes@gmail.com>
2022-06-09 16:35:49 -0300

    feat / add scroll effect on other pages
    add scroll effect on other pages;
    add mobile view;
    fix bugs;

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let controller;
 let slideScene;
 let pageScene;
+let detailScene;
 
 function animateSlides() {
     // Init Controller
@@ -23,7 +24,6 @@ function animateSlides() {
         slideTl.fromTo(revealImg, { x: '0%' }, { x: '100%' });
         slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, '-=1');
         slideTl.fromTo(revealText, { x: '0%'}, { x: '100%'}, '-=0.75');
-        slideTl.fromTo(nav, { y: '-100%' }, { y: '0%' }, '-=0.9');
 
         // Create Scene
         slideScene = new ScrollMagic.Scene({
@@ -134,8 +134,12 @@ barba.init({
             namespace: 'fashion',
             beforeEnter(){
                 logo.href = '../index.html';
-                gsap.fromTo('.nav-header', 1, { y: '100%' }, { y: '0%', ease: 'power2.inOut' });
+                detailAnimation();
             },
+            beforeLeave(){
+                controller.destroy();
+                detailScene.destroy();
+            }
         }
     ],
     transitions: [
@@ -157,12 +161,42 @@ barba.init({
                 const tl = gsap.timeline({defaults: { ease: 'power2.inOut'}});
                 tl.fromTo('.swipe', 0.75, { x: '0%'}, { x: '100%', stagger: 0.25, onComplete: done });
                 tl.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1 });
+                tl.fromTo('.nav-header', 1, { y: '-100%' }, { y: '0%', ease: 'power2.inOut' }, "-=1.5");
             }
         }
     ]
 })
 
-// Events
+function detailAnimation() {
+    controller = new ScrollMagic.Controller();
+    const slides = document.querySelectorAll('.detail-slide');
+    slides.forEach((slide, index, slides) => {
+        const slideTl = gsap.timeline({ defaults: { duration: 1 } });
+        let nextSlide = slides.length - 1 === index ? 'end' : slides[index + 1];
+        const nextImg = nextSlide.querySelector('img');
+        slideTl.fromTo(slide, {opacity: 1}, { opacity: 0});
+        slideTl.fromTo(nextSlide, {opacity: 0}, {opacity: 1}, "-=1");
+        slideTl.fromTo(nextImg, {x: '50%'}, {x: '0%'})
+
+        // Scene
+        detailScene = new ScrollMagic.Scene({
+            triggerElement: slide,
+            duration: '100%',
+            triggerHook: 0
+        })
+        .setPin(slide, {pushFollowers: false})
+        .setTween(slideTl)
+        .addIndicators({
+            colorStart: "white",
+            colorTrigger: "white",
+            name: "detailScene"
+        })
+        .addTo(controller)
+
+    })
+}
+
+// EventsListeners
 burger.addEventListener('click', navToggle);
 window.addEventListener('mousemove', cursor);
 window.addEventListener('mouseover', activeCursor);
